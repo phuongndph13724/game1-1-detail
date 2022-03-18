32f2ab067c464c25ab00b8ff41e87c35262d8856
dacphuongotp <dacphuongotp@gmail.com>
2022-03-18 10:44:44 +0700

    first

---
+++ assets/Scripts/GameManager.ts
@@ -0,0 +1,293 @@
+import {
+  _decorator,
+  Component,
+  Node,
+  Prefab,
+  CCInteger,
+  instantiate,
+  Vec3,
+  Label,
+} from "cc";
+import { PlayerController } from "./PlayerController";
+const { ccclass, property } = _decorator;
+
+/**
+ * Predefined variables
+ * Name = GameManager
+ * DateTime = Wed Mar 16 2022 09:28:43 GMT+0700 (Indochina Time)
+ * Author = dacphuongotp
+ * FileBasename = GameManager.ts
+ * FileBasenameNoExtension = GameManager
+ * URL = db://assets/Scripts/GameManager.ts
+ * ManualUrl = https://docs.cocos.com/creator/3.4/manual/en/
+ *
+ */
+enum BlockType {
+  /**
+   * Khai báo một object có 2 thuộc tính
+   * BT_NONE = 0
+   * BT_STONE = 1
+   */
+  BT_NONE,
+  BT_STONE,
+}
+enum GameState {
+  /**
+   * B16
+   * Thêm logic trạng thái trò chơi, nói chung nó có thể được chia thành ba trạng thái:
+   * Init : hiển thị menu trò chơi và khởi tạo một số tài nguyên.
+   * Chơi : ẩn menu trò chơi, người chơi có thể vận hành trò chơi.
+   * Kết thúc : kết thúc trò chơi và hiển thị menu kết thúc.
+   *
+   */
+  GS_INIT,
+  GS_PLAYING,
+  GS_END,
+}
+
+@ccclass("GameManager")
+export class GameManager extends Component {
+  /**
+   * B11: Khởi tạo thuộc tính cubePrfb cho Prefab cube có giá trị null
+   * Khởi tạo thuộc tính roadLength (độ dài của đường nhảy có giá trị là 100)
+   * khai báo thuộc tính _road có kiểu dữ liệu là mội mảng Number[] có giá trị bằng rỗng
+   */
+  @property({ type: Prefab })
+  public cubePrfb: Prefab | null = null;
+  @property({ type: CCInteger })
+  public roadLength: Number = 1000;
+
+  @property({type: Label})
+  public stepsLabel: Label | null = null;
+  // B26-1 Khởi tạo Component stepsLabel cho GameManager
+
+  @property({ type: PlayerController })
+  public playerCtrl: PlayerController = null;
+  //   B19: Khởi tạo PlayerController cho GameManager
+
+  @property({ type: Node })
+  public startMenu: Node = null;
+  /**
+   * B20
+   * Để tự động mở / đóng menu đang mở, StartMenu cần phải tham chiếu đến GameManager.
+   *  Kéo StartMenu khung cảnh vào biến này trong bảng StartMenu
+   *  Kéo Player khung cảnh vào biến này trong bảng PlayCtrl
+   * Sửa đổi mã trong phương thức start (),init(), set curState()
+   */
+
+  private _road: number[] = [];
+  //   khai báo thuộc tính đường nhảy
+  private _curState: GameState = GameState.GS_INIT;
+  // khai báo thuộc tính đại diện cho trạng thái game
+  // [1]
+  // dummy = '';
+
+  // [2]
+  // @property
+  // serializableDummy = 0;
+
+  start() {
+    // this.generteRoad();
+    // //B13 gọi dến hàm generteRoad() để chạy
+    // B22-1 : Trỏ đến hành động curState có giá trị bằng GameState.GS_INIT để bắt đầu game
+    this.curState = GameState.GS_INIT;
+
+    this.playerCtrl?.node.on("JumpEnd", this.onPlayerJumpEnd, this);
+    // B25-1 Thêm sự kiện kết thúc trò chơi cho game bằng hàm onPlayerJumpEnd
+  }
+
+  /**
+   * B21
+   * Khởi tạo phương thức init() bắt đầu cho player
+   * 
+   
+   */
+  init() {
+    if (this.startMenu) {
+      this.startMenu.active = true;
+      // nếu có startMenu thì bắt đầu sẽ bằng sự kiện click chuột
+    }
+    this.generteRoad();
+    if (this.playerCtrl) {
+      // nếu có playerCtrl thì không bắt đầu game và cho player đứng im
+      this.playerCtrl.setInputActive(false);
+      this.playerCtrl.node.setPosition(Vec3.ZERO);
+    }
+    this.playerCtrl.reset();
+    // B25-4 : gọi đến phương thức reset game
+  }
+  /**
+   * B26: Tạo một label mới có tên Steps trong Canvas có type string là 0
+   * B26-2 Kéó Steps vào StepLabel trong GameManager và sửa đổi trong set curState()
+   */
+
+  /**
+   * B22
+   * Khởi tạo hành động curState có giá trị là GameState
+   *
+   */
+  set curState(value: GameState) {
+    switch (value) {
+      // tạo vòng lặp game
+      case GameState.GS_INIT:
+        this.init();
+        break;
+      // nếu value là GameState.GS_INIT thì trỏ đến phương thức init() để start trò chơi
+
+      case GameState.GS_PLAYING:
+        if (this.startMenu) {
+          this.startMenu.active = false;
+          // nếu có startMenu ở GameManager (scene)  thì
+          // trỏ đến setInputActive(active: boolean) ở PlayController chuyển thành giá trị là False
+        }
+
+        if(this.stepsLabel){
+          // B26-3 Đặt lại số bước đã nhảy được bằng 0
+          this.stepsLabel.string = '0';
+
+        }
+
+
+        setTimeout(() => {
+          if (this.playerCtrl) {
+            this.playerCtrl.setInputActive(true);
+            // nếu có playerCtrl thì trỏ đến setInputActive(active: boolean) ở PlayController
+            // để thực hiện hành động start click chuột để bắt đầu game
+          }
+        }, 0.1);
+        break;
+      case GameState.GS_END:
+        break;
+    }
+    this._curState = value;
+    // trỏ đến trạng thái _curState có giá trị là các case trong switch(case);
+  }
+
+  /**
+   * B23: Khởi tạo phương thức click vào button thì sẽ bắt đầu game
+   * Gán GameManager cho PlayButton trong scene - GameManager - onStartButtonClickde()
+   */
+  onStartButtonClicked() {
+    this.curState = GameState.GS_PLAYING;
+  }
+
+  generteRoad() {
+    /**
+     * B12 Khởi tạo phương thức : tạo các đoạn đường nhảy cho player
+     */
+    this.node.removeAllChildren();
+    this._road = [];
+    // trỏ đến thuộc tính đường nhảy
+    // startPos bắt đầu position
+    this._road.push(BlockType.BT_STONE);
+
+    for (let i = 1; i < this.roadLength; i++) {
+      // i là các vị trí của khối đường nhảy
+      if (this._road[i - 1] === BlockType.BT_NONE) {
+        //   nếu i đường nhảy - 1 = 0 thì =>đoạn đường nhảy sẽ bằng 1
+        this._road.push(BlockType.BT_STONE);
+      } else {
+        this._road.push(Math.floor(Math.random() * 2));
+        // khởi tạo các đoạn đường có khoảng cách tối đa bằng 1
+      }
+    }
+
+    for (let j = 0; j < this._road.length; j++) {
+      let block: Node = this.spawnBlockByType(this._road[j]);
+      if (block) {
+        //   nếu có block thì
+        this.node.addChild(block);
+        block.setPosition(j, -1.5, 0);
+        // console.log(j);
+      }
+    }
+  }
+
+  spawnBlockByType(type: BlockType) {
+    /**
+     * B14  khởi taih biến block được trỏ tới phương thức spawnBlockByType có giá trị truyền vào là một _road[j]
+     *
+     * B15 Tạo một menu bắt đầu đơn giản bắt đầu
+     * Tạo một nút button có chế độ xem là 2D trong scene
+     * Thêm khung nền bằng cách tạo một Sprite nút có tên BG bên dưới StartMenu
+     * Tạo một nút
+     *
+     */
+    if (!this.cubePrfb) {
+      return null;
+    }
+    let block: Node | null = null;
+    switch (type) {
+      case BlockType.BT_STONE:
+        block = instantiate(this.cubePrfb);
+        break;
+      //     console.log("hàm spawnBlockByType có giá trị block");
+      //   console.log(block);
+    }
+    return block;
+  }
+
+  /**
+   * B25
+   * Tăng logic thất bại và kết thúc để đánh giá cách trò chơi đang được chơi.
+   * Nếu Player nhảy đến một ô trống hoặc vượt quá giá trị độ dài tối đa, trò chơi sẽ kết thúc
+   */
+  checkResoult(moveIndex: number) {
+    if (moveIndex <= this.roadLength) {
+      // nếu moveIndex mà nhỏ hơn hoặc bằng độ dài đoạn đường nhảy
+      if (this._road[moveIndex] === BlockType.BT_NONE) {
+        this.curState = GameState.GS_INIT;
+        // nếu thuộc tính đường nhảy của moveIndex === 0 thì trạng thái game sẽ chuyển về lúc mới đầu
+      }
+    } else {
+      // bỏ qua độ dài tối đa
+      this.curState = GameState.GS_INIT;
+      // ngược lại :  thuộc tính đường nhảy của moveIndex === 0 thì trạng thái game sẽ chuyển về lúc mới đầu
+    }
+  }
+
+  onPlayerJumpEnd(moveIndex: number) {
+
+
+    this.stepsLabel.string = '' + moveIndex;
+    // B26-4 : Cộng thêm số điểm sau mỗi bước nhảy
+
+    this.checkResoult(moveIndex);
+    // B25-2 Khai báo phương thức kết thúc là trỏ đến phương thức checkResoult có giá trị là moveIndex
+
+  }
+
+  /**
+   * B27 : Thêm bóng đổ cho nhân vật
+   * Trong bảng Hệ thống phân cấp , hãy nhấp vào Scene nút ở trên cùng,
+   *  kiểm tra Enabled thuộc shadows tính và sửa đổi các thông số Distance và Normal
+   * Sửa ShadowColor thành màu đen và sửa Normal thành (0,0,1); distance thành -1
+   * Nhấn vào nút phân cấp Body chuyển ShadowCastingMode thành ON 
+   * Lúc này chưa nhìn thấy bóng của player => Nhấn vào nut Main Light chuyển Rotation thành (-10,17,0)
+   * 
+   */
+
+  /**
+   * B28 : Dowload Hình ảnh game cho phayer
+   * Kéo folder cocos vào assets 
+   * Kéo Cocos trong cocos vào bên trong Body
+   * Remove cc.MeshRenderer
+   * Created Spot Light trong Body => B29 bên PlayerController
+   * 
+   */
+
+  // update (deltaTime: number) {
+  //     // [4]
+  // }
+}
+
+/**
+ * [1] Class member could be defined like this.
+ * [2] Use `property` decorator if your want the member to be serializable.
+ * [3] Your initialization goes here.
+ * [4] Your update function goes here.
+ *
+ * Learn more about scripting: https://docs.cocos.com/creator/3.4/manual/en/scripting/
+ * Learn more about CCClass: https://docs.cocos.com/creator/3.4/manual/en/scripting/decorator.html
+ * Learn more about life-cycle callbacks: https://docs.cocos.com/creator/3.4/manual/en/scripting/life-cycle-callbacks.html
+ */
---
+++ assets/Scripts/PlayerController.ts
@@ -0,0 +1,209 @@
+import {
+  _decorator,
+  Component,
+  Node,
+  Animation,
+  Vec3,
+  input,
+  Input,
+  EventMouse,
+  SkeletalAnimation,
+} from "cc";
+const { ccclass, property } = _decorator;
+
+/**
+ * Predefined variables
+ * Name = PlayerController
+ * DateTime = Mon Mar 14 2022 09:49:12 GMT+0700 (Indochina Time)
+ * Author = dacphuongotp
+ * FileBasename = PlayerController.ts
+ * FileBasenameNoExtension = PlayerController
+ * URL = db://assets/Scripts/PlayerController.ts
+ * ManualUrl = https://docs.cocos.com/creator/3.4/manual/en/
+ *
+ */
+
+@ccclass("PlayerController")
+export class PlayerController extends Component {
+  // [1]
+  // dummy = '';
+  // B7 Khởi tạo thành phần hoạt ảnh Animation
+  @property({ type: Animation })
+  public BodyAnim: Animation | null = null;
+  // [2]
+  // @property
+  // serializableDummy = 0;
+
+  @property({ type: SkeletalAnimation })
+  public CocosAnim: SkeletalAnimation | null = null;
+  /**
+   * B29: Khởi tạo thành phần cho Hoạt ảnh Player
+   * Kéo Cocos trong Body vào trong CocosAnim của Player
+   * B29-1 : Sửa đổi trong phương thức jumpByStep()
+   */
+
+  // B1:  Khởi tạo thuộc tính của đối tượng PlayerController
+  private _startJump: boolean = false;
+  // khởi tạo thuộc tính đầu khi start game có giá trị là false;
+  private _jumpStep: number = 0;
+  //  khởi tạo thuộc tính bước nhảy cho player
+  private _curJumpTime: number = 0;
+  //  khởi tạo thời gian hiện tại của bước nhảy
+  private _jumpTime: number = 0.4;
+  // khởi tạo tốc độ bật nhảy của player
+  private _curJumpSpeed: number = 0;
+  // khởi tạo tốc độ thời gian của bước nhảy
+  private _curPos: Vec3 = new Vec3();
+  // khởi tạo vị trí hiện tại của Player là từ địa điểm của một Vec3 mới
+  private _deltaPos: Vec3 = new Vec3(0, 0, 0);
+  // khởi tạo
+  private _targetPos: Vec3 = new Vec3();
+  // khởi tạo vị trí của player khi thực hiện bước nhảy là một Vec3 mới
+  private _isMoving = false;
+  // khởi tạo biến di chuyển cho player có giá trị là false
+
+  private _curMoveIndex = 0;
+  // B24-1: Khởi tạo điểm số đạt được khi chơi có giá trị khởi tạo ban đầu bằng 0
+
+  start() {
+    // [3]
+    (window as any).player = this;
+    // input.on(Input.EventType.MOUSE_UP, this.onMouseUp, this); (B17)
+    //B2: khởi tạo hành động từ người dùng nhập vào ở phương thức onMouseUp
+    // B17: bật tắt động tính năng theo dõi tin nhắn chuột của nhân vật.
+  }
+  reset() {
+    this._curMoveIndex = 0;
+    // B25-3 : Phương thức reset game khi trò chơi kết thúc => gọi phương thức này ra tại
+    // phương thức init trong GameManager
+  }
+  /**
+   * B18:
+   */
+  setInputActive(active: boolean) {
+    if (active) {
+      console.log(active);
+      // Nếu active === true thì thêm sự kiện click chuột cho player
+      input.on(Input.EventType.MOUSE_UP, this.onMouseUp, this);
+    } else {
+      input.off(Input.EventType.MOUSE_UP, this.onMouseUp, this);
+    }
+  }
+
+  onMouseUp(event: EventMouse) {
+    // B3:  khởi tạo phương thức onMouseUp có biến là event type EventMouse từ "cc"
+    if (event.getButton() === 0) {
+      this.jumpByStep(1);
+      //   nếu lấy event giá trị trả về = 0 thì trỏ đến phương thức jumpByStep có giá trị là 1
+    } else if (event.getButton() === 2) {
+      this.jumpByStep(2);
+      //   nếu lấy event giá trị trả về = 2 thì trỏ đến phương thức jumpByStep có giá trị là 2
+    }
+  }
+
+  jumpByStep(step: number) {
+    //B4:   Khởi tạo phương thức jumpByStep có biến là step type number
+    if (this._isMoving) {
+      return;
+      // nếu có biến _isMoving thì không có giá trị trả về
+    }
+    this._startJump = true; // trỏ đến biến _startJump đổi thành giá trị true
+    this._jumpStep = step; // trỏ đến biến _jumpStep đổi thành giá trị step
+    this._curJumpTime = 0; // trỏ đến biến _curJumpTime
+    this._curJumpSpeed = this._jumpStep / this._jumpTime;
+    // biến đổi thành _jumpStep/_jumpTime
+    this.node.getPosition(this._curPos); // trả về giá trị là một Vec3 mới
+    Vec3.add(this._targetPos, this._curPos, new Vec3(this._jumpStep, 0, 0));
+
+    this._isMoving = true; // Chuyển đổi không di chuyển thành có di chuyển
+
+    if (this.CocosAnim) {
+      // B29-1 : Nếu có CocosAnim thì  Hoạt ảnh nhảy mất nhiều thời gian, đây là tốc độ phát lại
+      // =>  onOnceJumpEnd()
+      this.CocosAnim.getState("cocos_anim_jump").speed = 3.5;
+
+      /*
+        getState (tên: string): AnimationState;
+         / **
+          * @en
+          * Tạo trạng thái cho clip được chỉ định.
+          * Nếu đã có một clip có cùng tên, trạng thái hoạt ảnh hiện có sẽ bị dừng và bị ghi đè.
+          * @en
+          * Tạo trạng thái hoạt hình bằng clip hoạt hình được chỉ định.
+          * Nếu trạng thái hoạt ảnh với tên được chỉ định đã tồn tại, trạng thái hoạt ảnh hiện có trước tiên sẽ được đặt thành dừng và ghi đè.
+          * @param clip Clip hoạt hình
+          * @param name Tên trạng thái hoạt ảnh, nếu không có, tên của clip mặc định sẽ được sử dụng
+          * @returns Trạng thái hoạt ảnh được tạo
+          * /
+      */
+      // Phát hoạt ảnh nhảy
+      this.CocosAnim.play("cocos_anim_jump");
+    }
+
+    // B8: Thêm mã phát lại hoạt ảnh vào chức năng nhảy jumpByStep
+    if (this.BodyAnim) {
+      if (step === 1) {
+        this.BodyAnim.play("oneStep");
+      } else if (step === 2) {
+        this.BodyAnim.play("twoStep");
+      }
+      // B9 Nâng cấp đường
+      /**
+       * Tạo một folder có tên là Pfabs
+       * Kéo khối hình ảnh Cube đầu tiên xuống
+       */
+      /**
+       * B10 Tạo một file .ts trong folder Scripts có tên là GameManager
+       * => file GameManager.ts
+       */
+    }
+    this._curMoveIndex += step;
+    // B24-2 : trỏ đến _curMoveIndex có giá trị += bước nhảy
+  }
+
+  onOnceJumpEnd() {
+    //B5 Khởi tạo phương thức kết thúc cho đối tượng
+    this._isMoving = false; // trỏ thành k di chuyển
+
+    // B29-2 : xong sửa đổi trạng thái chờ trong phương thức onOnceJumpEnd()
+    if (this.CocosAnim) {
+      this.CocosAnim.play("cocos_anim_idle");
+    }
+
+    this.node.emit("JumpEnd", this._curMoveIndex);
+    // B24-3 : Gửi tin nhắn mỗi bước nhảy mỗi lần kết thúc trò chơi
+  }
+
+  update(deltaTime: number) {
+    //B6 khởi tạo phương thức update player có biến là detaTime type là number
+    if (this._startJump) {
+      // nếu có biến _startJump =>
+      this._curJumpTime += deltaTime; // trỏ đến biến _curJumpTime += deltaTime
+      if (this._curJumpTime > this._jumpTime) {
+        // end
+        this.node.setPosition(this._targetPos);
+        this._startJump = false;
+        this.onOnceJumpEnd();
+        // console.log(this._curJumpTime);
+        // console.log(this._jumpTime);
+      } else {
+        // tween
+        this.node.getPosition(this._curPos);
+        this._deltaPos.x = this._curJumpSpeed * deltaTime;
+        Vec3.add(this._curPos, this._curPos, this._deltaPos);
+        this.node.setPosition(this._curPos);
+      }
+    }
+  }
+}
+
+/**
+ * [1] Class member could be defined like this.
+ * [2] Use `property` decorator if your want the member to be serializable.
+ * [3] Your initialization goes here.
+ * [4] Your update function goes here.
+ *
+ * Learn more about scripting: https://docs.cocos.com/creator/3.4/manual/en/scripting/
+ * Learn more about CCClass: https://docs.cocos.com/creator/3.4/manual/en/scripting/decorator.html
+ * Learn more about life-cycle callbacks: https://docs.cocos.com/creator/3.4/manual/en/scripting/life-cycle-callbacks.html
+ */
